e050cbd5d1e7bd585f61e3ada2e9e557432ea273
Guillermo Narvaez-Paliza <gunarvae@tibco.com>
2019-05-25 15:33:40 -0400

    updated SHA

--- studioutils.rb
+++ studioutils.rb
@@ -2,7 +2,7 @@ class Studioutils < Formula
     desc "A shell script with utilities for TIBCO StreamBase Studio developers."
     homepage "https://github.com/gunarvae/studioutils"
     url "https://github.com/gunarvae/studioutils/blob/master/archive/studioutils-1.0.0.tar.gz"
-    sha256 "3692d73c72680e5e1bd6745097b0b1df9a2886b83cf826b4a89503a9a08f73af"
+    sha256 "0d343abe37cc4ac200679002fc8a6dee5d30d9eb2150196d74e6c706c25be9bf"
     version "1.0.0"
 
     bottle :unneeded
