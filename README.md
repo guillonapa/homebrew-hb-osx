4c387c8db0f1d6f4b015aa4c0604137a46e34c0d
Guillermo Narvaez-Paliza <gunarvae@tibco.com>
2019-05-25 14:28:48 -0400

    added studioutils formulae

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # homebrew-hb-osx
+
+Personal public homebrew tap for OS X homebrew formulae.
---
+++ studioutils.rb
@@ -0,0 +1,13 @@
+class Studioutils < Formula
+    desc "A shell script with utilities for TIBCO StreamBase Studio developers."
+    homepage "https://github.com/gunarvae/studioutils"
+    url "https://github.com/gunarvae/studioutils/raw/master/archive/studioutils-1.0.0.tar.gz"
+    sha256 "071b760cd75a140f6a8a5216f3d86b98d7ac2397c8c42a68681770b8b79af274"
+    version "1.0.0"
+
+    bottle :unneeded
+
+    def install
+        bin.install "studioutils"
+    end
+end
