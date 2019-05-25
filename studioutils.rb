fe4dc6afb8f9321365a300aa2bb3b2b0cc665e61
Guillermo Narvaez-Paliza <gunarvae@tibco.com>
2019-05-25 14:51:42 -0400

    corrected url to tarball

--- studioutils.rb
+++ studioutils.rb
@@ -1,7 +1,7 @@
 class Studioutils < Formula
     desc "A shell script with utilities for TIBCO StreamBase Studio developers."
     homepage "https://github.com/gunarvae/studioutils"
-    url "https://github.com/gunarvae/studioutils/raw/master/archive/studioutils-1.0.0.tar.gz"
+    url "https://github.com/gunarvae/studioutils/blob/master/archive/studioutils-v1.0.0.tar.gz"
     sha256 "071b760cd75a140f6a8a5216f3d86b98d7ac2397c8c42a68681770b8b79af274"
     version "1.0.0"
 
