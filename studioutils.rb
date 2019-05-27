01a399968d813b411a82f384e50e8be94b9953d9
Guillermo Narvaez-Paliza <gunarvae@tibco.com>
2019-05-27 12:39:39 -0400

    modified formula... modifying shasum

--- studioutils.rb
+++ studioutils.rb
@@ -2,7 +2,7 @@ class Studioutils < Formula
     desc "A shell script with utilities for TIBCO StreamBase Studio developers."
     homepage "https://github.com/gunarvae/studioutils"
     url "https://github.com/gunarvae/studioutils/archive/v1.0.0.tar.gz"
-    sha256 "bcd784afa893e89fa717f4575d59ae9e8d1dfb06cdba78eaea0b5152f70c2e40"
+    sha256 "9b46a81cf516de1716c0f7b1c0d15a2bd26f0d07e94ca79dc2df1ba36ac1e1a4"
     version "1.0.0"
 
     bottle :unneeded
