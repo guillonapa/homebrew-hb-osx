e382828556d9de1775f5674d137fe17027eb55aa
Guillermo Narvaez-Paliza <guillonapa@gmail.com>
2020-02-11 11:44:40 -0500

    updated shasum

--- studioutils.rb
+++ studioutils.rb
@@ -2,7 +2,7 @@ class Studioutils < Formula
     desc "A shell script with utilities for TIBCO StreamBase Studio developers."
     homepage "https://github.com/gunarvae/studioutils"
     url "https://github.com/guillonapa/studioutils/archive/v1.2.0.tar.gz"
-    sha256 "a40ca64b35627406c10ba65739e89bf33385ea5cf06d66275c721939f6f31e15"
+    sha256 "efb21a2088a08a2e915118669c2c3b8c580d19c5b6021a2764c8867bdcb78d0e"
     version "1.2.0"
 
     bottle :unneeded
