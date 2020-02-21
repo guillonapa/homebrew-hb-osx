23cc1d73a84a1592e553ef21406e424f3804b99f
Guillermo Narvaez-Paliza <guillonapa@gmail.com>
2020-02-21 10:20:21 -0500

    bumping version to 1.2.1

--- studioutils.rb
+++ studioutils.rb
@@ -3,7 +3,7 @@ class Studioutils < Formula
     homepage "https://github.com/gunarvae/studioutils"
     url "https://github.com/guillonapa/studioutils/archive/v1.2.0.tar.gz"
     sha256 "c68f1db9774065d291e7aff3fbe0e20d8958d4ce48176a741eb2d8303dd82442"
-    version "1.2.0"
+    version "1.2.1"
 
     bottle :unneeded
 
