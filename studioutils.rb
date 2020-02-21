1bc74c924f09683207ce35167a08b2614114aa44
Guillermo Narvaez-Paliza <guillonapa@gmail.com>
2020-02-21 10:23:20 -0500

    bumping version to 1.2.1

--- studioutils.rb
+++ studioutils.rb
@@ -1,7 +1,7 @@
 class Studioutils < Formula
     desc "A shell script with utilities for TIBCO StreamBase Studio developers."
     homepage "https://github.com/gunarvae/studioutils"
-    url "https://github.com/guillonapa/studioutils/archive/v1.2.0.tar.gz"
+    url "https://github.com/guillonapa/studioutils/archive/v1.2.1.tar.gz"
     sha256 "c68f1db9774065d291e7aff3fbe0e20d8958d4ce48176a741eb2d8303dd82442"
     version "1.2.1"
 
